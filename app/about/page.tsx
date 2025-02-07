b99bd7339c81cb5b8621d67cab08f30b7525dad1
gowrishan2 <gowrishangararl@gmail.com>
2025-02-07 23:35:13 +0530

    changes commited

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,7 +6,7 @@ export const metadata = {
 export default function About() {
 
     return <main className="container mx-auto px-4 py-6">
-    <h2 className="text-4xl font-bold mb-4">About Us</h2>
-    <p>Next js Learning by projects...</p>
-</main>
+            <h2 className="text-4xl font-bold mb-4">About Us</h2>
+            <p>Learning Next js by doing projects...</p>
+         </main>
 }
